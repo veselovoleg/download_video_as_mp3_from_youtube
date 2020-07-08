1720aac0bb4965512c3dd88ab020ca370f126dfa
veselovoleg <veselovoleg.nn@gmail.com>
2020-07-08 14:19:39 +0200

    init

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import re
+import time
+import pafy
+
+
+def main():
+    print("Insert link to YouTube video")
+    url = input()
+    link_match = check_youtube_link(url)
+
+    if link_match is not None:
+        download_video(url)
+    else:
+        print("Incorrect link!")
+
+
+def download_video(download_url):
+    video = None
+    start_time = time.time()
+
+    # noinspection PyBroadException
+    try:
+        video = pafy.new(download_url)
+    finally:
+        if video is not None:
+            print(f"'{video.title}'. Length: {video.duration}.")
+            print("Downloading started")
+
+            best_audio = video.getbestaudio()
+            best_audio.download(filepath=f"/{os.getcwd()}/{video.title}.mp3")
+
+            print("--- %s seconds ---" % (time.time() - start_time))
+            print(f"'{video.title}' downloaded")
+        else:
+            print("Error occurred.")
+
+
+def check_youtube_link(link):
+    pattern = re.compile("^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$")
+    return pattern.match(link)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
